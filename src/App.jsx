edd9c23848c11ccb568eb8e8975b64f989eb1753
Victoria <mvperera@gmail.com>
2023-06-14 14:25:37 -0300

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Navbar from "./Common/Navbar/Navbar";
 
 function App() {
   return (
-   <>
-   </>
-  )
+    <>
+      <Navbar />
+    </>
+  );
 }
 
-export default App
+export default App;
