16e4a6ce9469d66fb544c80f82087088a86964c3
Victoria <mvperera@gmail.com>
2023-06-14 15:50:09 -0300

    navbar responsive

--- src/Common/Navbar/UnicornNavbar.jsx
+++ src/Common/Navbar/UnicornNavbar.jsx
@@ -8,27 +8,26 @@ function UnicornNavbar() {
   return (
     <>
       <Navbar bg="light" expand="lg" sticky="top">
-        <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Container fluid>
+          <Navbar.Brand href="#home">
+            <img src="" alt="Unicorn Logo" />
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              <Nav className="me-auto">
+                <Nav.Link href="#">ABOUT OUR PROJECT</Nav.Link>
+                <Nav.Link href="#">OUR BEER</Nav.Link>
+                <Nav.Link href="#">SHOP</Nav.Link>
+                <Nav.Link href="#" className="py-1 px-2">
+                  <i class="bi bi-person-fill fs-5"></i>
+                </Nav.Link>
+                <Nav.Link href="#" className="py-1 px-2">
+                  <i class="bi bi-cart-fill fs-5"></i>
+                </Nav.Link>
+                <Nav.Link href="#">CONTACT</Nav.Link>
+              </Nav>
+            </Navbar.Text>
           </Navbar.Collapse>
         </Container>
       </Navbar>
