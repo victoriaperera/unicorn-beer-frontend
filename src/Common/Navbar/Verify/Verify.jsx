24e5e0fc03ba851a663e3a3a327c8853df1712f9
estebanCastaneira <estebanc_b@hotmail.com>
2023-06-14 18:43:21 -0300

    Verify

--- src/Common/Navbar/Verify/Verify.jsx
+++ src/Common/Navbar/Verify/Verify.jsx
@@ -9,60 +9,62 @@ function Verify() {
   const [birthMonth, setMonth] = useState();
   const [birthYear, setYear] = useState();
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const isOldEnough = useVerifyAge(birthDay, birthMonth, birthYear)
-    console.log(isOldEnough)
     if(isOldEnough){
-      handleClose(); 
       setShow(false);
     }  
   }
   return(
     <Modal show={show} size="lg" className='unicornModal'>
-            <Modal.Header className='border-0 d-flex justify-content-start' >
-              <Modal.Title className='me-5'><img alt='Unicorn Logo'/></Modal.Title>
-              <h2>Please, tell us: When where you born?</h2>
+            <Modal.Header className="border-0 d-flex flex-column">
+              <img alt='Unicorn Logo'/>
+              <Modal.Title className='me-5'>
+                <h3 className='mt-3'>Please, tell us: When where you born?</h3>
+              </Modal.Title>
             </Modal.Header>
             <Modal.Body>
-              <Form className='d-flex justify-content-center fs-5' onSubmit={handleSubmit}>
-                <InputGroup className="m-2">
-                  <Form.Control
-                    className='text-center month'
-                    placeholder="MM"
-                    aria-label="Month"
-                    name="month"
-                    onChange={(e) => setMonth(e.target.value)}
-                  />
-                 </InputGroup>
-                 <InputGroup className="m-2">
-                  <Form.Control
-                    className='text-center day'
-                    placeholder="DD"
-                    aria-label="Day"
-                    name="day"
-                    onChange={(e) => setDay(e.target.value)}
-                  />
-                 </InputGroup>
-                 <InputGroup className="m-2">
-                  <Form.Control
-                    className='text-center year'
-                    placeholder="YYYY"
-                    aria-label="Year"
-                    name="year"
-                    onChange={(e) => setYear(e.target.value)}
-                  />
+              <Form onSubmit={handleSubmit}>
+                <div className='d-flex'>
+                  <InputGroup className="m-2 inputDate">
+                    <Form.Control
+                      className='text-center month'
+                      placeholder="MM"
+                      aria-label="Month"
+                      name="month"
+                      onChange={(e) => setMonth(e.target.value)}
+                    />
                   </InputGroup>
-                 <InputGroup className="m-2">
-                  <Form.Control
+                  <InputGroup className="m-2 inputDate">
+                    <Form.Control
+                      className='text-center day'
+                      placeholder="DD"
+                      aria-label="Day"
+                      name="day"
+                      onChange={(e) => setDay(e.target.value)}
+                    />
+                  </InputGroup>
+                  <InputGroup className="m-2 inputDate">
+                    <Form.Control
+                      className='text-center year'
+                      placeholder="YYYY"
+                      aria-label="Year"
+                      name="year"
+                      onChange={(e) => setYear(e.target.value)}
+                    />
+                    </InputGroup>
+                </div>
+                <div>
+                  <InputGroup className="m-2">
+                   <Form.Control
                     className='text-center btn-primary'
                     value="ENTER"
                     type="submit"
-                  />
+                    />
                   </InputGroup>
+                </div>
               </Form>
             </Modal.Body>
         
