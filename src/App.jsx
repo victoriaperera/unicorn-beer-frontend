66c2ca6c377f944e926c932cf5f8453de73b6092
estebanCastaneira <estebanc_b@hotmail.com>
2023-06-15 11:25:51 -0300

    sigue Verify

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <>
       <UnicornNavbar />
-      {/* <Verify/> */}
+      <Verify/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
