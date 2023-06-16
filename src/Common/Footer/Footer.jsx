c30b95872adf80b313fb746d62acfedbcf6929ec
Nicolas Martinez <52309078+Martinez-Latorraca@users.noreply.github.com>
2023-06-16 10:37:18 -0300

    footer fix

--- src/Common/Footer/Footer.jsx
+++ src/Common/Footer/Footer.jsx
@@ -1,55 +1,45 @@
 import "./styles.css";
-import React from "react";
-import { Col, NavDropdown, NavLink, Nav, Row } from "react-bootstrap";
+import { NavLink, Nav } from "react-bootstrap";
+
 function Footer() {
   return (
-  <footer className="footer container-fluid">
-    <div className="d-flex justify-content-between align-items-center">
-      <NavLink href="/">
-              <img
-                src="/src/assets/icons/Unicorn-beer-white-logo-iso.svg"
-                alt="Unicorn Logo"
-                className="icon-beer d-inline"
-              />
-      </NavLink>
-      <div>
-        <Nav className="d-none d-sm-block d-flex flex-column">
-          <Nav.Link href="/about" className="mx-2 mt-1 footerLink text-white">
-            ABOUT OUR PROJECT
-          </Nav.Link>
-          <Nav.Link href="#" className="mx-2 mt-1 footerLink text-white">
-            OUR BEER
-          </Nav.Link>
-          <Nav.Link href="/shop" className="mx-2 mt-1 footerLink text-white">
-            SHOP
-          </Nav.Link>
-          <NavDropdown
-            title={<i className="bi bi-person-fill fs-5 text-white"></i>}
-            id="basic-nav-dropdown"
-            className="p-0 mx-2"
-          >
-            <NavDropdown.Item href="#">Sign in</NavDropdown.Item>
-            <NavDropdown.Item href="#">Create account</NavDropdown.Item>
-            <NavDropdown.Item href="#">My account</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#">Log out</NavDropdown.Item>
-          </NavDropdown>
-          <Nav.Link href="/contact" className="mx-2 mt-1 footerLink text-white">
-            CONTACT
-          </Nav.Link>
-        </Nav>
+    <footer className="footer container-fluid">
+      <div className="d-flex justify-content-between align-items-center">
+        <NavLink href="/">
+          <img
+            src="/src/assets/icons/Unicorn-beer-white-logo-iso.svg"
+            alt="Unicorn Logo"
+            className="icon-beer d-inline"
+          />
+        </NavLink>
+        <div>
+          <Nav className=" d-flex flex-column">
+            <Nav.Link href="/about" className="mx-2 mt-1 footerLink text-white">
+              ABOUT OUR PROJECT
+            </Nav.Link>
+            <Nav.Link href="#" className="mx-2 mt-1 footerLink text-white">
+              OUR BEER
+            </Nav.Link>
+            <Nav.Link href="/shop" className="mx-2 mt-1 footerLink text-white">
+              SHOP
+            </Nav.Link>
+
+            <Nav.Link href="/contact" className="mx-2 mt-1 footerLink text-white">
+              CONTACT
+            </Nav.Link>
+          </Nav>
+        </div>
+        <div>
+          <h4 className="text-white">Our Social:</h4>
+          <div className="d-flex justify-content-around">
+            <i className="bi bi-facebook text-white"></i>
+            <i className="bi bi-instagram text-white"></i>
+            <i className="bi bi-twitter text-white"></i>
+          </div>
+        </div>
       </div>
-      <div>
-        <h4 className="text-white">Our Social:</h4>
-        <div className="d-flex justify-content-around">
-          <i class="bi bi-facebook text-white"></i>
-          <i class="bi bi-instagram text-white"></i>
-          <i class="bi bi-twitter text-white"></i>
-        </div>   
-      </div>
-    </div>
-  </footer>
-  )
+    </footer>
+  );
 }
 
 export default Footer;
