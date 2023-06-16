251d0069ef7a0d033713a698cafb3b3928e05d4b
Victoria <mvperera@gmail.com>
2023-06-16 11:52:57 -0300

    product view

--- src/Features/Product/Product.jsx
+++ src/Features/Product/Product.jsx
@@ -28,7 +28,7 @@ function Product() {
   return (
     <div className="container-fluid product-view-bg">
       <div className="row">
-        <div className="col-12 col-md-6 product-view-img">
+        <div className="col-12 col-md-5 product-view-img">
           <Carousel
             variant="dark"
             activeIndex={activeIndex}
@@ -43,21 +43,21 @@ function Product() {
             </Carousel.Item>
           </Carousel>
         </div>
-        <div className="col-12 col-md-6 align-self-center px-5">
+        <div className="col-12 col-md-7 align-self-center px-5 product-view-text">
           <div>
-            <h2 className="h4 pb-2">
+            <h2 className="pb-2">
               IPA Bottle 16.91 Oz
               <span className="badge rounded-pill text-bg-warning fs-6 ms-3">NEW</span>
             </h2>
             <p className="lh-lg">Some beer description here.</p>
             <hr />
-            <p className="fs-3 fw-bold">$9.99</p>
           </div>
-          <div className="addToCartBtn-product">
+          <div className="pb-3">
+            <p className="fs-2 fw-bold">$9.99</p>
             <AddToCardBtn />
           </div>
           <div className="d-flex align-items-center pt-2">
-            <i className="bi bi-truck fs-5 text-white me-2"></i>
+            <i className="bi bi-truck fs-4 text-white me-2"></i>
             <p className="m-0">Delivery available.</p>
           </div>
         </div>
