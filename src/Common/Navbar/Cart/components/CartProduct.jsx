773bfd16f0f1fc99d4b4d93ba62e2e96b1faaa1b
Nicolas Martinez <52309078+Martinez-Latorraca@users.noreply.github.com>
2023-06-16 16:32:32 -0300

    cart fixes

--- src/Common/Navbar/Cart/components/CartProduct.jsx
+++ src/Common/Navbar/Cart/components/CartProduct.jsx
@@ -7,13 +7,18 @@ function CartProduct({ product }) {
   const img = useCheckImg(product.photos);
 
   return (
-    <div className="cart-product">
+    <div className="cart-product my-3">
       <div className="">
         <img src={img[0]} alt="Product image" className="cart-img" />
       </div>
-      <div className="">
-        <p>{product.name}</p>
-        <p>${product.price}</p>
+      <div>
+        <div className="mt-2">
+          <span>{product.name}</span>
+          <span className="ms-2">${product.price}</span>
+        </div>
+        <div className="mt-2">
+          <span className="fw-bold">{product.quantity}</span>
+        </div>
       </div>
     </div>
   );
