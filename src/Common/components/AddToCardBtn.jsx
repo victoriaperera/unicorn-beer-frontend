00b2745fe84174a94df0cf5504b23e77963f5a48
Nicolas Martinez <52309078+Martinez-Latorraca@users.noreply.github.com>
2023-06-16 19:27:11 -0300

    Add to cart btn fix

--- src/Common/components/AddToCardBtn.jsx
+++ src/Common/components/AddToCardBtn.jsx
@@ -1,19 +1,41 @@
 import { useDispatch } from "react-redux";
 import "./styles.css";
-import { Button } from "react-bootstrap";
 import { addToCart } from "../Navbar/Cart/cartSlice";
+import { useSetProductColor } from "../../hook/useSetProductColor";
+import { useState } from "react";
+import { Button } from "react-bootstrap";
 
 function AddToCardBtn({ product }) {
   const dispatch = useDispatch();
+  const color = useSetProductColor(product.style.name);
+  const [isHovering, setIsHovering] = useState(false);
+
+  console.log(product);
+
+  const handleMouseEnter = () => {
+    setIsHovering(true);
+    console.log(isHovering);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovering(false);
+    console.log(isHovering);
+  };
+
   return (
     <>
-      <Button
-        size="lg"
-        className="rounded-pill addtocart-btn mt-2"
+      <button
+        className="rounded-pill addtocart-btn mt-2  "
         onClick={() => dispatch(addToCart(product))}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        style={{
+          backgroundColor: isHovering ? "white" : "transparent",
+          color: isHovering ? color : "white",
+        }}
       >
         Buy Now
-      </Button>
+      </button>
     </>
   );
 }
