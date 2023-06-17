651453267f98599d797692e122e9ee274ba9d920
Victoria <mvperera@gmail.com>
2023-06-17 11:39:38 -0300

    about text

--- src/Features/About/Components/AboutHeader.jsx
+++ src/Features/About/Components/AboutHeader.jsx
@@ -9,18 +9,21 @@ function AboutHeader() {
       <div className="overlay-cans">
         <div className="gradient-overlay"></div>
         <Row className="content ps-5">
-          <Col md={5} lg={6} xl={4}>
+          <Col md={6} lg={6} xl={5}>
             <h2>What is Unicorn Beer?</h2>
             <p>
-              Unicorn Beer is an app developed using the MERN stack as the final project of a Coding
-              Bootcamp at Hack Academy, an educational institution specializing in programming
-              courses. The Bootcamp is an immersive and intensive full-time course that lasts for
-              three months and requires a workload of 600 hours. Its goal is to prepare its students
-              to be Junior Full Stack Developers.
+              Unicorn Beer is an e-commerce app developed using the MERN stack as the final project
+              of a Coding Bootcamp at Hack Academy, an educational institution that specializes in
+              programming courses. The Bootcamp is an immersive and intensive full-time course that
+              lasts for three months and requires a workload of 600 hours. Its goal is to prepare
+              its students to be Junior Full Stack Developers.
               <hr className="border-0" />
               This final project aims to showcase the application of the diverse technologies
               learned throughout the Bootcamp. Unicorn Beer was developed over a period of three
               weeks by a team of five students, totaling approximately 700 hours of work.
+              <hr className="border-0" />
+              Unicorn beer is a fully functional app designed to be user-friendly and easy to
+              navigate.
             </p>
           </Col>
         </Row>
