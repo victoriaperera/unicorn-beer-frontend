a98a540fbe8b4fdf5da987ab63a72e3f1d9255ef
estebanCastaneira <estebanc_b@hotmail.com>
2023-06-19 16:58:27 -0300

    send an email Work In Process

--- src/Features/Contact/Contact.jsx
+++ src/Features/Contact/Contact.jsx
@@ -1,66 +1,84 @@
 import "./styles.css";
 import { Button, Col, Container, Form, Row, InputGroup } from "react-bootstrap";
-import React from "react";
+import { useState } from "react";
+import axios from "axios";
 
 function Contact() {
+  const [firstname, setFirstname] = useState("")
+  const [lastname, setLastname] = useState("")
+  const [email, setEmail] = useState("")
+  const [phone, setPhone] = useState("")
+  const [message, setMessage] = useState("")
+
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    try {
+      const response = axios({
+        method: "POST",
+        url: "http://localhost:3000/auth/contact",
+        data: {
+          firstname,
+          lastname,
+          email,
+          phone,
+          message
+        }
+      })
+      console.log(response);
+    } catch(err) {
+      console.log(err);
+    }
   };
   return (
     <>
       <div className="contact p-5">
-        <Container className="contactContainer py-5 container">
-          <div className="d-flex justify-content-start align-items-center">
+        <Container className="contactContainer my-5 py-5 container">
+          <div className="d-flex align-items-end">
             <img
               src="src/assets/icons/Unicorn-beer-icon-3.svg"
               alt="unicron icon"
-              className="contactIcon"
+              className="contactIcon me-3"
             />
-            <h2 className="py-5 ms-3">Leave Us a message</h2>
+            <h2 className="m-0">Leave Us a message</h2>
           </div>
-          <Form onSubmit={handleSubmit}>
+          <Form method="POST" onSubmit={handleSubmit}>
             <Row className="mb-3">
               <Form.Group as={Col} md="4" className="my-2">
                 <Form.Label>First name</Form.Label>
-                <Form.Control required type="text" placeholder="First name" />
-                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                <Form.Control required type="text" placeholder="First name"
+                onChange={(e)=> setFirstname(e.target.value)}/>
               </Form.Group>
               <Form.Group as={Col} md="4" className="my-2">
                 <Form.Label>Last name</Form.Label>
-                <Form.Control required type="text" placeholder="Last name" />
-                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                <Form.Control required type="text" placeholder="Last name" 
+                onChange={(e)=> setLastname(e.target.value)}/>
               </Form.Group>
               <Form.Group as={Col} md="4" className="my-2">
                 <Form.Label>Email</Form.Label>
                 <InputGroup hasValidation>
-                  <Form.Control type="email" placeholder="Email" required />
+                  <Form.Control type="email" placeholder="Email" required 
+                  onChange={(e)=> setEmail(e.target.value)}/>
                   <Form.Control.Feedback type="invalid">
                     Please put a contact e-mail.
                   </Form.Control.Feedback>
                 </InputGroup>
               </Form.Group>
+              <Form.Group as={Col} md="4" className="my-2">
+                <Form.Label>Phone</Form.Label>
+                  <Form.Control type="number" placeholder="Phone"
+                  onChange={(e)=> setPhone(e.target.value)}/>
+              </Form.Group>
             </Row>
             <Row className="mb-3">
-              <Form.Group as={Col} md="6" className="my-2">
-                <Form.Label>City</Form.Label>
-                <Form.Control type="text" placeholder="City" required />
-                <Form.Control.Feedback type="invalid">
-                  Please provide a valid city.
-                </Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group as={Col} md="3" className="my-2">
-                <Form.Label>State</Form.Label>
-                <Form.Control type="text" placeholder="State" required />
-                <Form.Control.Feedback type="invalid">
-                  Please provide a valid state.
-                </Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group as={Col} md="3" className="my-2">
-                <Form.Label>Zip</Form.Label>
-                <Form.Control type="text" placeholder="Zip" required />
-                <Form.Control.Feedback type="invalid">
-                  Please provide a valid zip.
-                </Form.Control.Feedback>
+              <Form.Group as={Col}  className="my-2">
+                <Form.Label>Message</Form.Label>
+                <Form.Control 
+                required 
+                as="textarea" 
+                style={{height: "200px"}}
+                onChange={(e)=> setMessage(e.target.value)}
+                />
               </Form.Group>
             </Row>
             <Row className="justify-content-end">
@@ -68,7 +86,7 @@ function Contact() {
                 type="submit"
                 variant="outline-light"
                 size="lg"
-                className="rounded-pill w-25 me-3 mt-5"
+                className="rounded-pill w-25 me-3 mt-4"
               >
                 Send
               </Button>
