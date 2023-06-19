a8af45b49d4b90dc1292ff7d9602e7d646e10f4a
estebanCastaneira <estebanc_b@hotmail.com>
2023-06-19 15:18:10 -0300

    w-75

--- src/Features/Err404/Err404.jsx
+++ src/Features/Err404/Err404.jsx
@@ -5,7 +5,7 @@ function Err404() {
     <div className="err404Container">
       <div className="container d-flex justify-content-center align-items-center position-relative">
         <h1 className="z-1">Oops!!! Page Not Found</h1>
-        <img src="/src/assets/img/unicronAtBeach.jpg" alt="lost unicorn" className="w-50 rounded position-absolute"/>
+        <img src="/src/assets/img/unicronAtBeach.jpg" alt="lost unicorn" className="w-75 rounded position-absolute"/>
       </div>
     </div>
   )
