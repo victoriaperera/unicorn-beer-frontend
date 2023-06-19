64201449cb69d28f8cd1ebd95ed7efd63c5b154b
Lorena Rincon <lorec.rincon@gmail.com>
2023-06-19 16:46:06 -0300

    Product photos

--- src/Features/Dashboard/components/Board.jsx
+++ src/Features/Dashboard/components/Board.jsx
@@ -25,30 +25,59 @@ function Board() {
 
   return (
     <>
-      <div className="col-5 text-center pt-4 background">
-        <div className="rounded-start-4 bg-secondary w-100 my-3 py-5">
-          <h5 className="card-title">Cantidad de ventas Totales</h5>
-          <p className="card-text">1.543</p>
+      <div className="col-5 text-center background-graphite pt-4 text-white ">
+        <div class="background border-2 shadow p-1 w-100 mt-3">
+          <div class="card-body">
+            <img src="/src/assets/img/grafica3.png" alt="grafica" className="grafica" />
+          </div>
         </div>
-        <div className="rounded-start-4 bg-secondary w-100 my-3 py-5">
-          <h5 className="card-title">Promedio de ventas por semana</h5>
-          <p className="card-text">124</p>
-        </div>
-        <div className="rounded-start-4 bg-secondary w-100 my-3 py-5">
-          <h5 className="card-title">Cantidad de usuarios</h5>
-          <p className="card-text">638</p>
-        </div>
-        <div className="rounded-start-4 bg-secondary w-100 my-3 py-5">
-          <h5 className="card-title">Promedio de usuarios registrados por semana</h5>
-          <p className="card-text">40</p>
-        </div>
-        <div className="rounded-start-4 bg-secondary w-100 my-3 py-5">
-          <h5 className="card-title">Cantidad de Productos en stock</h5>
-          <p className="card-text">{productCount}</p>
+        <div class="row mt-4">
+          <div class="col-sm-6 mb-3 mb-sm-0">
+            <div class="background border-2 shadow p-5 mt-3">
+              <div class="card-body">
+                <h5 className="card-title">Cantidad de ventas</h5>
+                <span className="card-text">1.543</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6">
+            <div class="background border-2 shadow p-5 mt-3">
+              <div class="card-body">
+                <h5 className="card-title">Cantidad de Productos</h5>
+                <span className="card-text">{productCount}</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6">
+            <div class="background border-2 shadow p-5 mt-3">
+              <div class="card-body">
+                <h5 className="card-title">Total de clientes</h5>
+                <span className="card-text">654</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6">
+            <div class="background border-2 shadow p-5 mt-3">
+              <div class="card-body">
+                <h5 className="card-title">Ingresos de hoy</h5>
+                <span className="card-text">$7.560</span>
+              </div>
+            </div>
+          </div>
+          <div class="card-body mt-3">
+            <img src="/src/assets/img/grafica3.png" alt="grafica" className="grafica" />
+          </div>
         </div>
       </div>
-      <div className="col-4 background d-flex justify-content-center align-items-center p-0 pe-5">
-        <div className="rounded-end-4 bg-secondary w-100 h-75 my-3 py-5 margin-top"></div>
+      <div className="col-4 background-graphite d-flex justify-content-center align-items-center p-0 pe-5 text-white">
+        <div className="border-2 background shadow w-100 h-75 my-3 py-5 margin-top text-center">
+          <h3>Productos</h3>
+          <img
+            src="/src/assets/img/Scottish_bottle.png"
+            alt="Scottish_bottle"
+            className="scottish mt-4"
+          />
+        </div>
       </div>
     </>
   );
