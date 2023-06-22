aef5e46dc800f5047933f332fae7a44501ab59fe
Nicolas Martinez <52309078+Martinez-Latorraca@users.noreply.github.com>
2023-06-22 11:00:22 -0300

    cart global variable fix

--- src/Common/Navbar/Cart/components/CartFooter.jsx
+++ src/Common/Navbar/Cart/components/CartFooter.jsx
@@ -1,32 +1,30 @@
 import "./styles.css";
 import { Link } from "react-router-dom";
-import Offcanvas from "react-bootstrap/Offcanvas";
 import { useDispatch, useSelector } from "react-redux";
-import React, { useEffect, useState } from "react";
-import { setTotal } from "../cartSlice";
+import { useEffect } from "react";
+import { setTotalAmount } from "../cartSlice";
 
 function CartFooter({ handleClose }) {
   const cart = useSelector((state) => state.cart.products);
   const user = useSelector((state) => state.user);
+  const totalAmount = useSelector((state) => state.cart.totalAmount);
   const dispatch = useDispatch();
 
-  const calculateTotal = () => {
+  useEffect(() => {
     let total = 0;
     if (Array.isArray(cart)) {
       cart.map((product) => {
         total += product.price * product.quantity;
       });
-      dispatch(setTotal({ totalAmount: total.toFixed(2) }));
-      return total.toFixed(2);
+      dispatch(setTotalAmount({ totalAmount: total.toFixed(2) }));
     }
-    return total;
-  };
+  }, [cart]);
 
   return (
     <div className="border-top cart-footer">
       <div className="d-flex justify-content-between px-2">
         <p className="fw-bold">Order total</p>
-        <p className="fw-bold">${calculateTotal()}</p>
+        <p className="fw-bold">${totalAmount}</p>
       </div>
       <div className="d-flex justify-content-between px-2">
         <p className="fw-bold">Shipping</p>
