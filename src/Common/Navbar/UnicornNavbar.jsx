a9590e4f8d8ca1d7e9042d8232d0f796945e0708
Lorena Rincon <lorec.rincon@gmail.com>
2023-06-23 14:31:19 -0300

    navbar

--- src/Common/Navbar/UnicornNavbar.jsx
+++ src/Common/Navbar/UnicornNavbar.jsx
@@ -15,7 +15,6 @@ import { setTotalQuantity } from "./Cart/cartSlice";
 function UnicornNavbar() {
   const [show, setShow] = useState(false);
   const [navbarBlur, setNavbarBlur] = useState("");
-  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
   const user = useSelector((state) => state.user);
   const cart = useSelector((state) => state.cart.products);
   const totalQuantity = useSelector((state) => state.cart.totalQuantity);
@@ -54,13 +53,9 @@ function UnicornNavbar() {
     <>
       <Navbar collapseOnSelect expand="lg" fixed="top" className={`custom-navbar ${navbarBlur}`}>
         <Container fluid className="collapsed-nav">
-          <Navbar.Toggle
-            aria-controls="responsive-navbar-nav"
-            className="nav-toggler-btn"
-            onClick={() => setNavbarBlur("navbar-blur")}
-          />
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" className="nav-toggler-btn" />
           <Navbar.Brand>
-            <Nav.Link to={"/"}>
+            <Nav.Link as={Link} to={"/"}>
               <img
                 src="/src/assets/icons/Unicorn-beer-white-logo-iso.svg"
                 alt="Unicorn Logo"
@@ -69,61 +64,60 @@ function UnicornNavbar() {
             </Nav.Link>
           </Navbar.Brand>
           <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
-            <Navbar.Text>
-              <Nav className="ms-auto">
-                <div className="mx-2 mt-1">
-                  <Nav.Link href="/about" className="nav-link">
-                    ABOUT OUR PROJECT
-                  </Nav.Link>
-                </div>
-                <div className="mx-2 mt-1">
-                  <Nav.Link href="/#our-beer-section" className="nav-link">
-                    OUR BEER
-                  </Nav.Link>
-                </div>
-                <div className="mx-2 mt-1">
-                  <Nav.Link
-                    href="/shop"
-                    className="nav-link"
-                    onClick={() => dispatch(clearFilter())}
-                  >
-                    SHOP
-                  </Nav.Link>
-                </div>
-                <NavDropdown
-                  title={<i className="bi bi-person-fill fs-5 text-white"></i>}
-                  id="basic-nav-dropdown"
-                  className="mx-2"
+            <Nav className="ms-auto">
+              <div className="mx-2 mt-1">
+                <Nav.Link as={Link} to={"/about"} className="nav-link">
+                  ABOUT OUR PROJECT
+                </Nav.Link>
+              </div>
+              <div className="mx-2 mt-1">
+                <Nav.Link as={Link} to={"/#our-beer-section"} className="nav-link">
+                  OUR BEER
+                </Nav.Link>
+              </div>
+              <div className="mx-2 mt-1">
+                <Nav.Link
+                  as={Link}
+                  to={"/shop"}
+                  className="nav-link"
+                  onClick={() => dispatch(clearFilter())}
                 >
-                  {!user && <NavDropdown.Item href="/login">Log in</NavDropdown.Item>}
+                  SHOP
+                </Nav.Link>
+              </div>
+              <NavDropdown
+                title={<i className="bi bi-person-fill fs-5 text-white"></i>}
+                id="basic-nav-dropdown"
+                className="mx-2"
+              >
+                {!user && <NavDropdown.Item href="/login">Log in</NavDropdown.Item>}
 
-                  {!user && <NavDropdown.Item href="/signup">Create an Account</NavDropdown.Item>}
+                {!user && <NavDropdown.Item href="/signup">Create an Account</NavDropdown.Item>}
 
-                  {!user && <NavDropdown.Item href="/admin/login">Admins</NavDropdown.Item>}
+                {!user && <NavDropdown.Item href="/admin/login">Admins</NavDropdown.Item>}
 
-                  {user && <NavDropdown.Item href="#">My account</NavDropdown.Item>}
+                {user && <NavDropdown.Item href="#">My account</NavDropdown.Item>}
 
-                  {user && (
-                    <NavDropdown.Item href="#">
-                      <Logout></Logout>
-                    </NavDropdown.Item>
-                  )}
-                </NavDropdown>
-                <div className="mx-2 mt-2">
-                  <i className="cartIcon bi bi-cart-fill fs-5 text-white" onClick={handleShow}>
-                    <sup className="superscript">({totalQuantity})</sup>
-                  </i>
-                  <Offcanvas show={show} onHide={handleClose} placement="end">
-                    <Cart show={show} handleClose={handleClose} />
-                  </Offcanvas>
-                </div>
-                <div className="mx-2 mt-1">
-                  <Nav.Link href="/contact" className="nav-link">
-                    CONTACT
-                  </Nav.Link>
-                </div>
-              </Nav>
-            </Navbar.Text>
+                {user && (
+                  <NavDropdown.Item href="#">
+                    <Logout></Logout>
+                  </NavDropdown.Item>
+                )}
+              </NavDropdown>
+              <div className="mx-2 mt-2">
+                <i className="cartIcon bi bi-cart-fill fs-5 text-white" onClick={handleShow}>
+                  <sup className="superscript">({totalQuantity})</sup>
+                </i>
+                <Offcanvas show={show} onHide={handleClose} placement="end">
+                  <Cart show={show} handleClose={handleClose} />
+                </Offcanvas>
+              </div>
+              <div className="mx-2 mt-1">
+                <Nav.Link as={Link} to={"/contact"} className="nav-link">
+                  CONTACT
+                </Nav.Link>
+              </div>
+            </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
