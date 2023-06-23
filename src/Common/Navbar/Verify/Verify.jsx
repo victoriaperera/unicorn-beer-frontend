835858bf4295f0f789a871dd86ada33a0a6d2e94
Victoria <mvperera@gmail.com>
2023-06-23 17:42:57 -0300

    contact

--- src/Common/Navbar/Verify/Verify.jsx
+++ src/Common/Navbar/Verify/Verify.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Collapse, Form, InputGroup, Modal } from 'react-bootstrap';
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Collapse, Form, InputGroup, Modal } from "react-bootstrap";
 import "./styles.css";
-import { useVerifyAge } from '../../../hook/useVerifyAge';
-import { toggleModal } from './verifyAgeSlice';
+import { useVerifyAge } from "../../../hook/useVerifyAge";
+import { toggleModal } from "./verifyAgeSlice";
 
 function Verify() {
-
-  const show = useSelector( state => state.verify )
+  const show = useSelector((state) => state.verify);
   const dispatch = useDispatch();
   const [child, setChild] = useState();
   const [birthDay, setDay] = useState("");
@@ -16,82 +15,88 @@ function Verify() {
   const allInputsSeted = birthDay && birthMonth && birthYear.length > 3;
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(allInputsSeted){
-      const isOldEnough = useVerifyAge(birthDay, birthMonth, birthYear)
-      if(isOldEnough){
+    if (allInputsSeted) {
+      const isOldEnough = useVerifyAge(birthDay, birthMonth, birthYear);
+      if (isOldEnough) {
         dispatch(toggleModal());
-      }else{
-        setChild(true)
+      } else {
+        setChild(true);
       }
     }
-  }
-  return(
-        <Modal show={show} size="xl" backdrop="static" className='unicornModal'>
-            <Modal.Header className="border-0 d-flex flex-column justify-content-center">
-              <img src="src/assets/icons/Unicorn-beer-icon-black.svg"alt='Unicorn Logo' className='w-50'/>
-              <Modal.Title>
-                <h3 className='mt-5 text-center'>Please, tell us: When were you born?</h3>
-                {child && <small className='oops'>Oops! We're sorry, but you have to be an adult to get into this site</small>}
-              </Modal.Title>
-            </Modal.Header>
-            <Modal.Body className='mt-5'>
-              <Form onSubmit={handleSubmit}>
-                <div className='d-flex my-3'>
-                  <InputGroup className="m-2">
-                    <Form.Control
-                      id='month'
-                      className='dateInput'
-                      placeholder="MM"
-                      aria-label="Month"
-                      name="month"
-                      type="number"
-                      min={1}
-                      max={12}
-                      onChange={(e) => setMonth(e.target.value)}
-                    />
-                  </InputGroup>
-                  <InputGroup className="m-2">
-                    <Form.Control
-                      id='day'
-                      className='dateInput'
-                      placeholder="DD"
-                      aria-label="Day"
-                      name="day"
-                      type="number"
-                      min={1}
-                      max={31}
-                      onChange={(e) => setDay(e.target.value)}
-                    />
-                  </InputGroup>
-                  <InputGroup className="m-2">
-                    <Form.Control
-                      id='year'
-                      className='dateInput year'
-                      placeholder="YYYY"
-                      aria-label="Year"
-                      name="year"
-                      min={1900}
-                      onChange={(e) => setYear(e.target.value)}
-                    />
-                    </InputGroup>
-                </div>
-                <div className='d-flex justify-content-center'>
-                  <InputGroup className="my-4 w-50">
-                   <Form.Control
-                    className={
-                      allInputsSeted 
-                      ? "enterBtnActive" 
-                      : "enterBtnInactive"
-                    }
-                    value="enter"
-                    type="submit"
-                    />
-                  </InputGroup>
-                </div>
-              </Form>
-            </Modal.Body>
-        </Modal>
-  )
+  };
+
+  return (
+    <Modal show={show} size="md" centered backdrop="static" className="unicornModal">
+      <Modal.Header className="border-0 d-flex flex-column justify-content-center">
+        <img
+          src="src/assets/icons/Unicorn-beer-icon-black.svg"
+          alt="Unicorn Logo"
+          className="w-50 pt-3"
+        />
+        <Modal.Title>
+          <h3 className="mt-4 text-center">Please, tell us: When were you born?</h3>
+          {child && (
+            <small className="oops">
+              Oops! We're sorry, but you have to be an adult to get into this site
+            </small>
+          )}
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form onSubmit={handleSubmit}>
+          <div className="d-flex my-3">
+            <InputGroup className="m-2">
+              <Form.Control
+                id="month"
+                className="dateInput"
+                placeholder="MM"
+                autoFocus
+                aria-label="Month"
+                name="month"
+                type="number"
+                min={1}
+                max={12}
+                onChange={(e) => setMonth(e.target.value)}
+              />
+            </InputGroup>
+            <InputGroup className="m-2">
+              <Form.Control
+                id="day"
+                className="dateInput"
+                placeholder="DD"
+                aria-label="Day"
+                name="day"
+                type="number"
+                min={1}
+                max={31}
+                onChange={(e) => setDay(e.target.value)}
+              />
+            </InputGroup>
+            <InputGroup className="m-2">
+              <Form.Control
+                id="year"
+                className="dateInput year"
+                placeholder="YYYY"
+                aria-label="Year"
+                name="year"
+                min={1900}
+                onChange={(e) => setYear(e.target.value)}
+              />
+            </InputGroup>
+          </div>
+          <div className="d-flex justify-content-center">
+            <InputGroup className="my-2 w-50">
+              <Form.Control
+                className={allInputsSeted ? "enterBtnActive" : "enterBtnInactive"}
+                value="enter"
+                type="submit"
+              />
+            </InputGroup>
+          </div>
+        </Form>
+      </Modal.Body>
+    </Modal>
+  );
 }
 
 export default Verify;
---
+++ src/Features/Contact/components/ContactModal.jsx
@@ -0,0 +1,25 @@
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import React, { useState } from "react";
+
+function ContactModal() {
+  const [smShow, setSmShow] = useState(true);
+
+  return (
+    <>
+      <Modal
+        size="sm"
+        show={smShow}
+        onHide={() => setSmShow(false)}
+        aria-labelledby="example-modal-sizes-title-sm"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="example-modal-sizes-title-sm">Information</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>This feature is not within the project's scope.</Modal.Body>
+      </Modal>
+    </>
+  );
+}
+
+export default ContactModal;
