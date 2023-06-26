503400f73627e92e1032537268413e22e72b940a
Nicolas Martinez <52309078+Martinez-Latorraca@users.noreply.github.com>
2023-06-26 20:06:57 -0300

    Scroll to top from navbar

--- src/main.jsx
+++ src/main.jsx
@@ -3,20 +3,21 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import {store, persistor} from "./redux/store.js";
-import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
+import ScrollToTop from "./helpers/ScrollToTop";
+
 import "./index.css";
-
-
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
-     </Provider>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <ScrollToTop />
+          <App />
+        </PersistGate>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
-)
+);
