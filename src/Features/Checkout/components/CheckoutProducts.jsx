a3f4a416870e37d495cf85752c51f511f2f1bd7e
Victoria <mvperera@gmail.com>
2023-06-26 09:56:30 -0300

    checkout

--- src/Features/Checkout/components/CheckoutProducts.jsx
+++ src/Features/Checkout/components/CheckoutProducts.jsx
@@ -1,28 +1,24 @@
 import "./styles.css";
+import { useCheckImg } from "../../../hook/useCheckImg";
 import { useDispatch, useSelector } from "react-redux";
 import { useState } from "react";
-import Select from "react-select";
 import axios from "axios";
 
-function CheckoutProducts({ product }) {
-  const user = useSelector((state) => state.user);
-  const cart = useSelector((state) => state.cart);
-  const dispatch = useDispatch();
-
+function CheckoutProduct({ product }) {
+  const photos = useCheckImg(product.style.photos);
+  const main = photos.find(
+    (photo) => photo.includes("Main") && photo.includes(product.container.name),
+  );
   return (
     <div key={product.id} className="d-flex flex-row align-items-center mb-4">
-      <div className="w-25 me-2 text-center">
-        <img
-          src={`${import.meta.env.VITE_BACK_URL}/img/${product.photo}`}
-          alt={`${product.style.name} ${product.container.name}`}
-          className="checkout-img"
-        />
+      <div className="w-50 me-2 text-center">
+        <img src={main} alt={`${product.name} img`} className="checkout-img" />
       </div>
-      <div className="w-75 ps-2">
+      <div className="w-50 ps-2">
         <div className="d-flex flex-column checkout-product-details">
-          <span>{product.name}</span>
+          <span className="fw-bold">{product.name}</span>
           <span>
-            Quantity: {product.quantity} x US$ {product.price}
+            <span className="fw-bold text-orange">{product.quantity}</span> x US$ {product.price}
           </span>
           <span>Subtotal: US$ {(product.price * product.quantity).toFixed(2)}</span>
         </div>
@@ -31,4 +27,4 @@ function CheckoutProducts({ product }) {
   );
 }
 
-export default CheckoutProducts;
+export default CheckoutProduct;
