d60acc291e2c492e3a6ccd28e6a12321a0d1f5a9
estebanCastaneira <estebanc_b@hotmail.com>
2023-06-26 12:26:32 -0300

    product se actualiza en back

--- src/Features/Dashboard/components/ProductModalCU.jsx
+++ src/Features/Dashboard/components/ProductModalCU.jsx
@@ -59,18 +59,19 @@ function ProductModalCU({show, close, product, action}){
     }
     if(action === "edit"){
       try{
-        // const response = await axios({
-        //   method: "PATCH",
-        //   url: `${import.meta.env.VITE_BACK_URL}/products`,
-        //    // headers: {
-        //   //   Authorization: `Bearer ${token}`,
-        //   // },
-        //   data:{
-        //     productId: product.id,
-        //     price: price,
-        //     stock: stock
-        //   }
-        // })
+        const response = await axios({
+          method: "PATCH",
+          url: `${import.meta.env.VITE_BACK_URL}/products`,
+           // headers: {
+          //   Authorization: `Bearer ${token}`,
+          // },
+          data:{
+            productId: product.id,
+            price: price,
+            stock: stock
+          }
+        })
+        console.log(response);
         dispatch(updateProduct({productId: product.id, price: price, stock: stock}))
         close();
         resetStates();
