1a6cf15ecca6b05e18c4bc71e26068679fe6572a
Nicolas Martinez <52309078+Martinez-Latorraca@users.noreply.github.com>
2023-06-26 13:48:55 -0300

    out of product feature

--- src/Features/Product/components/ProductCard.jsx
+++ src/Features/Product/components/ProductCard.jsx
@@ -2,9 +2,11 @@ import "../styles.css";
 import AddToCartBtn from "../../../Common/components/AddToCartBtn";
 import { useDispatch } from "react-redux";
 import { useState } from "react";
+import { useNavigate } from "react-router";
 
 function ProductCard({ product }) {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const [counter, setCounter] = useState(1);
 
@@ -12,6 +14,10 @@ function ProductCard({ product }) {
     product.container === "keg"
       ? (product.container.volume * 1000 * 0.033814).toFixed(2)
       : (product.container.volume * 1000 * 0.000264172).toFixed(2);
+
+  const returnToShop = () => {
+    navigate("/shop");
+  };
 
   return (
     <div className="col-12 col-md-7 col-lg-6 align-self-center px-5 product-view-text">
@@ -33,29 +39,38 @@ function ProductCard({ product }) {
           <span className="fw-bold">US$ {product.price.toFixed(2)}</span>
         </div>
       </div>
-      <div className="w-100 d-flex justify-content-between border-top pt-2">
-        <div>
-          <div className="d-flex align-items-center me-3">
-            <i
-              className="bi bi-dash-circle"
-              onClick={() => {
-                counter > 1 && setCounter(counter - 1);
-              }}
-            ></i>
-            <span className="px-2 fw-bold">{counter}</span>
-            <i className="bi bi-plus-circle" onClick={() => setCounter(counter + 1)}></i>
-          </div>
-          {counter > 1 && (
-            <div className="">
-              <small>US$ {(product.price * counter).toFixed(2)}</small>
+      {product.stock > 0 ? (
+        <div className="w-100 d-flex justify-content-between border-top pt-2">
+          <div>
+            <div className="d-flex align-items-center me-3">
+              <i
+                className="bi bi-dash-circle"
+                onClick={() => {
+                  counter > 1 && setCounter(counter - 1);
+                }}
+              ></i>
+              <span className="px-2 fw-bold">{counter}</span>
+              <i className="bi bi-plus-circle" onClick={() => setCounter(counter + 1)}></i>
             </div>
-          )}
-        </div>
+            {counter > 1 && (
+              <div className="">
+                <small>US$ {(product.price * counter).toFixed(2)}</small>
+              </div>
+            )}
+          </div>
 
-        <div className="addToCartBtn-product d-flex justify-content-end">
-          <AddToCartBtn product={product} counter={counter} />
+          <div className="addToCartBtn-product d-flex justify-content-end">
+            <AddToCartBtn product={product} counter={counter} />
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className="w-100 d-flex justify-content-between border-top pt-2">
+          <span className="text-white">Out of stock</span>
+          <a className="text-white return-to-shop" onClick={returnToShop}>
+            Return to shop
+          </a>
+        </div>
+      )}
       <div className="d-flex align-items-center pt-2">
         <i className="bi bi-truck fs-6 text-white me-2"></i>
         <span className="m-0">Delivery available.</span>
