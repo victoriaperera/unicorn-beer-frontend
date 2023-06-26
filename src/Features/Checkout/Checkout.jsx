643022b40837b4259dbfa2ec7a8c818df4f04cbd
Victoria <mvperera@gmail.com>
2023-06-26 15:26:19 -0300

    checkout

--- src/Features/Checkout/Checkout.jsx
+++ src/Features/Checkout/Checkout.jsx
@@ -1,5 +1,6 @@
 import "./styles.css";
 import OrderModal from "./components/OrderModal";
+import CardForm from "./components/CardForm";
 import { Badge, Button, Container, Col, ListGroup, Form, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
@@ -8,7 +9,6 @@ import Select from "react-select";
 import axios from "axios";
 import { clearCart } from "../../Common/Navbar/Cart/cartSlice";
 import { Alert } from "react-bootstrap";
-import { capitalizeFirstLetter } from "../../hook/capitalizeFirstLetter";
 
 function Checkout() {
   const user = useSelector((state) => state.user);
@@ -16,6 +16,7 @@ function Checkout() {
   const dispatch = useDispatch();
   const [paymentMethod, setPaymentMet] = useState();
   const [deliveryDate, setDeliveryDate] = useState();
+  const [showCardForm, setShowCardForm] = useState(false);
 
   const [alertText, setAlertText] = useState("");
   const [alertToggle, setAlertToggle] = useState(null);
@@ -102,6 +103,11 @@ function Checkout() {
     }
   };
 
+  const handlePaymentMethodChange = (selectedOption) => {
+    setPaymentMet(selectedOption.value);
+    setShowCardForm(true);
+  };
+
   return (
     <div className="graphite-background d-flex justify-content-center align-items-center py-5">
       <OrderModal show={show} />
@@ -115,7 +121,7 @@ function Checkout() {
           <h2 className="m-0">Check Out</h2>
         </div>
         <div className="main-checkout">
-          <h3 className="pb-2">Ship to</h3>
+          <h3 className="pb-2">Deliver to</h3>
           <div className="mb-4">
             <span className="fw-bold">
               {user.firstname} {user.lastname}
@@ -125,7 +131,7 @@ function Checkout() {
             <div className="col-12 col-md-6">
               <div className="d-flex">
                 <h4>Shipping information </h4>
-                <i class="bi bi-pencil-square edit-info-icon"></i>
+                <i className="bi bi-pencil-square edit-info-icon"></i>
               </div>
 
               <div className="d-flex flex-column mb-3 read-only">
@@ -144,7 +150,7 @@ function Checkout() {
             <div className="col-12 col-md-6">
               <div className="d-flex">
                 <h4>Billing Information </h4>
-                <i class="bi bi-pencil-square edit-info-icon"></i>
+                <i className="bi bi-pencil-square edit-info-icon"></i>
               </div>
               <div className="d-flex flex-column mb-3 read-only">
                 <span>
@@ -163,89 +169,91 @@ function Checkout() {
         </div>
         <div className="main-checkout">
           <Form method="POST" onSubmit={handleSubmit}>
-            <h3>Payment</h3>
-            <Form.Group as={Col} md="6" className="my-2">
-              <Form.Label>
-                <i className="bi bi-credit-card-fill me-2"></i> Select Payment Method
-              </Form.Label>
-              <Select
-                name="paymentMethod"
-                options={paymentOptions}
-                styles={customStylesPM}
-                onChange={(e) => setPaymentMet(e.value)}
-                required
-              />
-            </Form.Group>
-            <Form.Group as={Col} md="6" className="my-2">
-              <Form.Label>
-                <i className="bi bi-truck fs-6 text-black me-2"></i> Select Delivery Date
-              </Form.Label>
-              <Select
-                name="deliveryDate"
-                options={deliveryOptions}
-                onChange={(e) => setDeliveryDate(e.value)}
-                styles={customStylesDD}
-              />
-            </Form.Group>
-
-            <div className="row mb-3">
-              <h3>Order details</h3>
-              <ListGroup as="ul" className="px-2">
-                {cart.products.length > 0 ? (
-                  cart.products.map((item) => {
-                    const photo = item.style.photos.filter(
-                      (photo) => photo.includes("Main") && photo.includes(`${item.container.name}`),
-                    );
-                    return (
-                      <ListGroup.Item
-                        key={item.id}
-                        as="li"
-                        className="d-flex flex-row align-items-center"
-                      >
-                        <div className="w-50 me-2 text-center">
-                          <img
-                            src={`${import.meta.env.VITE_BACK_URL}/img/${photo}`}
-                            alt={`${item.style.name} ${item.container.name}`}
-                            className="checkout-img"
-                          />
-                        </div>
-                        <div className="w-50 ps-2">
-                          <span className="m-0 fw-bold">{item.name}</span>
-                          <div className="d-flex flex-column checkout-product-details">
-                            <span>
-                              <span className="fw-bold text-orange">{item.quantity}</span> x US${" "}
-                              {item.price}
-                            </span>
+            <div className="row d-flex justify-content-between">
+              <div className="col-12 col-md-6">
+                <h3>Payment</h3>
+                <Form.Group className="my-2">
+                  <Form.Label>
+                    <i className="bi bi-credit-card-fill me-2"></i> Select Payment Method
+                  </Form.Label>
+                  <Select
+                    name="paymentMethod"
+                    options={paymentOptions}
+                    styles={customStylesPM}
+                    onChange={handlePaymentMethodChange}
+                    required
+                  />
+                </Form.Group>
+                {showCardForm && <CardForm user={user} />}
+                <Form.Group className="my-2">
+                  <Form.Label>
+                    <i className="bi bi-truck fs-6 text-black me-2"></i> Select Delivery Date
+                  </Form.Label>
+                  <Select
+                    name="deliveryDate"
+                    options={deliveryOptions}
+                    onChange={(e) => setDeliveryDate(e.value)}
+                    styles={customStylesDD}
+                  />
+                </Form.Group>
+              </div>
+              <div className="col-12 col-md-5">
+                <h3>Order details</h3>
+                <ListGroup as="ul" className="">
+                  {cart.products.length > 0 ? (
+                    cart.products.map((item) => {
+                      const photo = item.style.photos.filter(
+                        (photo) =>
+                          photo.includes("Main") && photo.includes(`${item.container.name}`),
+                      );
+                      return (
+                        <ListGroup.Item
+                          key={item.id}
+                          as="li"
+                          className="d-flex flex-row align-items-center"
+                        >
+                          <div className="w-50 me-2 text-center">
+                            <img
+                              src={`${import.meta.env.VITE_BACK_URL}/img/${photo}`}
+                              alt={`${item.style.name} ${item.container.name}`}
+                              className="checkout-img"
+                            />
                           </div>
-                        </div>
-                      </ListGroup.Item>
-                    );
-                  })
-                ) : (
-                  <ListGroup.Item
-                    as="li"
-                    className="d-flex justify-content-between align-items-start"
-                  >
-                    <div className="ms-2 me-auto">
-                      <p className="m-0 text-body-secondary">Your cart it's emtpy :(</p>
-                    </div>
-                    <Badge bg="danger" pill>
-                      0
-                    </Badge>
-                  </ListGroup.Item>
-                )}
-              </ListGroup>
-
-              <Col md="12" className="my-2 d-flex justify-content-end align-items-end">
-                <p className="m-0 me-2 fs-3 fw-bold text-orange">Total: </p>
-                <p className="m-0 fs-3 fw-bold">US$ {cart.totalAmount}</p>
-              </Col>
+                          <div className="w-50 ps-2">
+                            <span className="m-0 fw-bold">{item.name}</span>
+                            <div className="d-flex flex-column checkout-product-details">
+                              <span>
+                                <span className="fw-bold text-orange">{item.quantity}</span> x US${" "}
+                                {item.price}
+                              </span>
+                            </div>
+                          </div>
+                        </ListGroup.Item>
+                      );
+                    })
+                  ) : (
+                    <ListGroup.Item
+                      as="li"
+                      className="d-flex justify-content-between align-items-start"
+                    >
+                      <div className="ms-2 me-auto">
+                        <p className="m-0 text-body-secondary">Your cart it's emtpy :(</p>
+                      </div>
+                      <Badge bg="danger" pill>
+                        0
+                      </Badge>
+                    </ListGroup.Item>
+                  )}
+                </ListGroup>
+              </div>
             </div>
-            <Row className="">
-              <Button type="submit" variant="warning" className="rounded-pill w-25 mt-5">
+            <div className="my-3 d-flex flex-column">
+              <p className="m-0 me-2 fs-4 text-orange">Payment total: </p>
+              <p className="m-0 fs-4 fw-bold">US$ {cart.totalAmount}</p>
+              <Button type="submit" variant="warning" className="rounded-pill w-25 my-3">
                 Confirm Order
               </Button>
-              <span className="text-center">
+              <span className="">
                 <small>Our Damn Tasty Beer is Just a Click Away!</small>
               </span>
               {alertToggle && (
@@ -253,7 +261,7 @@ function Checkout() {
                   {alertText}
                 </Alert>
               )}
-            </Row>
+            </div>
           </Form>
         </div>
       </div>
