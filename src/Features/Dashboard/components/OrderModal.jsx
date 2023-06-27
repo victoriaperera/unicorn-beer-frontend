734d32b23e4b6ef80239ae53367e94345ee3b7fc
estebanCastaneira <estebanc_b@hotmail.com>
2023-06-27 11:51:01 -0300

    still improving

--- src/Features/Dashboard/components/OrderModal.jsx
+++ src/Features/Dashboard/components/OrderModal.jsx
@@ -12,33 +12,35 @@ function OrderModal({order, show, close}) {
         <Modal.Body>
             <ListGroup>
                 <ListGroup.Item action variant="light" as="li">
-                <span className="orderDetail">Customer:</span> {order.user.firstname} {order.user.lastname}  <span className="orderDetail">Email:</span> {order.user.email} <span className="orderDetail">Phone:</span>: {order.user.phone}
-                </ListGroup.Item>
-                <ListGroup.Item action variant="light" as="li">
-                <span className="orderDetail">Shipping Addres:</span> {order.user.shippingAddress}
+                <p><span className="orderDetail">Customer: </span>{order.user.firstname} {order.user.lastname}</p> 
+                <p><span className="orderDetail">Email: </span>{order.user.email}</p>
+                <p><span className="orderDetail">Phone: </span>{order.user.phone}</p>
                 </ListGroup.Item>
                 <div className="ms-2 my-2 me-auto">
                     <p className="m-0 orderDetail">Products:</p>
                 </div>
-                {order.products.map(product => 
-                 <ListGroup.Item
-                 as="li"
-                 className="d-flex justify-content-between align-items-start"
-                >
-                <div className="ms-2 me-auto">
-                    <div className="fw-bold">{product.name}</div>
-                 ${product.price}
-                </div>
-                 <Badge bg="primary" pill>
-                 {product.quantity}
-                 </Badge>
-             </ListGroup.Item>
+                {order.products.map(product =>
+                    <ListGroup.Item
+                    as="li"
+                    className="d-flex justify-content-between align-items-start"
+                   >
+                    <div className="ms-2 me-auto">
+                        <div className="fw-bold">{product.name}</div>
+                        ${product.price}
+                    </div>
+                        <Badge bg="primary" pill>
+                        {product.quantity}
+                        </Badge>
+                    </ListGroup.Item>
                     )}
                <ListGroup.Item action variant="light" as="li">
-                <span className="orderDetail">Total Quantity:</span> {order.totalQuantity} <span className="orderDetail">Payment Method:</span> {order.paymentMethod} <span className="orderDetail">Total:</span> ${order.totalAmount}
+                <p><span className="orderDetail">Total Quantity: </span>{order.totalQuantity}</p> 
+                <p><span className="orderDetail">Payment Method: </span>{order.paymentMethod}</p>  
+                <p><span className="orderDetail">Total: </span>${order.totalAmount}</p>
                </ListGroup.Item>
                <ListGroup.Item action variant="light" as="li">
-                <span className="orderDetail">Shipping Date:</span> {order.shippingDate}  <span className="orderDetail">Delivery Date:</span> {order.deliveryDate}
+                <p><span className="orderDetail">Shipping Date: </span>{order.shippingDate}</p>
+                <p><span className="orderDetail">Delivery Date: </span>{order.deliveryDate}</p> 
                 </ListGroup.Item>
                 <ListGroup.Item action variant="light" as="li">
                     <span className="orderDetail">{order.status}</span>
