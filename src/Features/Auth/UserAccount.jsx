e2127fe4da6e360a5b7ece47fd8007a9a1f33ebc
Victoria <mvperera@gmail.com>
2023-06-27 17:29:35 -0300

    user account Draft

--- src/Features/Auth/UserAccount.jsx
+++ src/Features/Auth/UserAccount.jsx
@@ -1,19 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-
 import UserOrder from "./components/UserOrder";
+import OutOfScopeModal from "../../Common/components/OutOfScopeModal";
 import "./styles.css";
 
 function UserAccount() {
   const user = useSelector((state) => state.user);
   const orders = user.orders;
 
+  const [showModal, setShowModal] = useState(false);
+
+  const handleEditPayment = () => {
+    setShowModal(true);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   console.log(orders);
 
   return (
     <div className="graphite-background-account d-flex justify-content-center align-items-center">
       <div className="container account-container">
-        <div className="d-flex justify-content-center align-items-center my-5">
+        <div className="d-flex justify-content-center align-items-center mb-5">
           <img
             src="src/assets/icons/Unicorn-beer-icon-3.svg"
             alt="unicorn icon"
@@ -24,28 +34,131 @@ function UserAccount() {
           </h2>
         </div>
         <div className="main-account">
-          <h3 className="mb-3">Account details</h3>
+          <h3 className="mb-3 heading-orange">Account details</h3>
           <div className="row">
-            <div className="col-12 col-md-6">
-              <h4>Shipping information</h4>
-              <form action="">
-                <label htmlFor="">Shipping address:</label>
-                <input type="text" defaultValue={user.shippingAddress} />
+            <div className="col-12 col-md-6 pe-5">
+              <form onSubmit={handleSubmit}>
+                <h4>Shipping & billing information</h4>
+                <div className="mb-2">
+                  <label htmlFor="" className="form-label">
+                    <small>Shipping address:</small>
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control form-control-sm"
+                    defaultValue={user.shippingAddress}
+                  />
+                </div>
+                <div className="mb-2">
+                  <label htmlFor="" className="form-label">
+                    <small>Billing address:</small>
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control form-control-sm"
+                    defaultValue={user.address}
+                  />
+                </div>
+                <div className="mb-2">
+                  <label htmlFor="" className="form-label">
+                    <small>Phone number:</small>
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control form-control-sm"
+                    defaultValue={user.phone}
+                  />
+                </div>
+                <button type="submit" className="btn btn-sm rounded-pill btn-outline-success mt-3">
+                  Save changes
+                </button>
               </form>
             </div>
-            <div className="col-12 col-md-6">
-              <h4>Billing information</h4>
+            <div className="col-12 col-md-6 pe-3">
+              <h4>My payment methods</h4>
+              <div className="mb-2">
+                <label htmlFor="" className="form-label">
+                  <small>Type:</small>
+                </label>
+                <input
+                  type="text"
+                  className="form-control form-control-sm"
+                  value="Credit Card"
+                  readOnly
+                  disabled
+                />
+              </div>
+              <div className="d-flex mb-2">
+                <div className="me-2 w-100">
+                  <label htmlFor="" className="form-label">
+                    <small>Card number:</small>
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control form-control-sm"
+                    value="9876-5432-1234-5678"
+                    readOnly
+                    disabled
+                  />
+                </div>
+                <div className="w-100">
+                  <label htmlFor="" className="form-label">
+                    <small>Card holder:</small>
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control form-control-sm"
+                    value={`${user.firstname} ${user.lastname}`}
+                    readOnly
+                    disabled
+                  />
+                </div>
+              </div>
+              <div className="d-flex mb-2">
+                <div className="me-2 w-100">
+                  <label htmlFor="" className="form-label">
+                    <small>Expiration date:</small>
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control form-control-sm"
+                    value="MM/AAAA"
+                    readOnly
+                    disabled
+                  />
+                </div>
+                <div className="w-100">
+                  <label htmlFor="" className="form-label">
+                    <small>CVV</small>
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control form-control-sm"
+                    value="123"
+                    readOnly
+                    disabled
+                  />
+                </div>
+              </div>
+              <button
+                type="button"
+                className="btn btn-sm rounded-pill btn-outline-primary mt-3"
+                onClick={handleEditPayment}
+              >
+                Edit payment
+              </button>
             </div>
           </div>
         </div>
         <div className="main-account">
-          <h3 className="mb-3">Order history</h3>
+          <h3 className="mb-3 heading-orange">Order history</h3>
           {orders && orders.length > 0 ? (
             orders.map((order) => <UserOrder key={order.id} order={order} />)
           ) : (
             <span>There are no orders yet. Lets shop!</span>
           )}
         </div>
+        {showModal && <OutOfScopeModal onClose={() => setShowModal(false)} />}
       </div>
     </div>
   );
--- src/Features/Auth/components/UserOrder.jsx
+++ src/Features/Auth/components/UserOrder.jsx
@@ -1,24 +1,38 @@
 import React from "react";
 import format from "date-fns/format";
+import Accordion from "react-bootstrap/Accordion";
 
 function UserOrder({ order }) {
+  const formatDate = (date) => {
+    return format(new Date(date), "dd-MMMM-yyyy");
+  };
+
   return (
-    <div className="d-flex flex-column mb-3 user-order">
-      <h5>
-        Order - {order.id} - {format(new Date(order.createdAt), "dd-MMMM-yyyy")}
-      </h5>
-      <span>Expected delivery date: {order.deliveryDate}</span>
-      <div>
-        {order.products.map((product) => (
-          <div key={product.id}>
-            <span>{product.name}</span>
-            <span>Quantity: {product.quantity}</span>
-            <span>Price: US$ {product.price}</span>
+    <Accordion>
+      <Accordion.Item eventKey={order.id.toString()} className="mb-3">
+        <Accordion.Header>
+          <h5>
+            Order: {order.id} | {formatDate(order.createdAt)}
+          </h5>
+        </Accordion.Header>
+        <Accordion.Body>
+          <div>
+            <span>Customer: {order.user.name} </span>
           </div>
-        ))}
-      </div>
-      <span>Total: US$ {order.totalAmount}</span>
-    </div>
+          <span>Expected delivery date: {formatDate(order.deliveryDate)}</span>
+          <div>
+            {order.products.map((product) => (
+              <div key={product.id}>
+                <span>{product.name}</span>
+                <span>Quantity: {product.quantity}</span>
+                <span>Price: US$ {product.price}</span>
+              </div>
+            ))}
+          </div>
+          <span>Total: US$ {order.totalAmount}</span>
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
   );
 }
 
