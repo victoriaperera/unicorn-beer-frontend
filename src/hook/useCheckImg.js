177262b8bdb738b64207734b5ee7bbd1303517f3
Victoria <mvperera@gmail.com>
2023-06-29 16:28:55 -0300

    bucket

--- src/hook/useCheckImg.js
+++ src/hook/useCheckImg.js
@@ -1,5 +1,5 @@
 export const useCheckImg = (imgs) => {
   const res = [];
-  imgs.map((img) => res.push(`${import.meta.env.VITE_BACK_URL}/img/${img}`));
+  imgs.map((img) => res.push(`${import.meta.env.VITE_BUCKET_URL}/img/${img}`));
   return res;
 };
