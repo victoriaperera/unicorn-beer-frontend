756d293710cb78e1ce5f3c170cc71fca007a85ba
Victoria <mvperera@gmail.com>
2023-06-30 10:42:23 -0300

    about

--- src/Features/About/Components/AboutHeader.jsx
+++ src/Features/About/Components/AboutHeader.jsx
@@ -51,7 +51,7 @@ function AboutHeader() {
                 project of a Coding Bootcamp at Hack Academy, an educational institution that
                 specializes in programming courses. The Bootcamp is an immersive and intensive
                 full-time course that lasts for three months and requires a workload of 600 hours.
-                Its goal is to prepare its students to be Junior Full Stack Developers.
+                Its goal is to prepare its students to be Full Stack Developers.
               </p>
               <p>
                 This final project aims to showcase the application of the diverse technologies
@@ -59,8 +59,8 @@ function AboutHeader() {
               </p>
               <p>
                 Unicorn Beer was developed over a period of three weeks by a team of five students,
-                totaling approximately 700 hours of work. It is a fully functional app designed to
-                be user-friendly and easy to navigate.
+                totaling approximately 150 hours of work per student. It is a fully functional app
+                designed to be user-friendly and easy to navigate.
               </p>
             </div>
           </Col>
--- src/Features/About/Components/DashboardSection.jsx
+++ src/Features/About/Components/DashboardSection.jsx
@@ -32,17 +32,16 @@ const DashboardSection = React.forwardRef((props, ref) => {
           <Col xs={12} md={8} lg={6} className="align-self-center mb-4">
             <h3 className="mb-3">ADMIN DASHBOARD</h3>
             <p>
-              In this e-commerce platform, we provide an admin panel dashboard where the
-              administrator has the ability to delete, update, or create products, categories and
-              users. Additionally, they can monitor the status of orders and make modifications as
-              necessary.
+              In this e-commerce platform, we provide an administrator panel dashboard that allows
+              the user to delete, update, or create products, categories, and customers.
+              Additionally, they can monitor the status of orders and make necessary modifications.
             </p>
           </Col>
           <Col className="d-flex image-container align-self-center justify-content-center p-0 col-12">
             <Image src={adminDashboard} alt="Admin dashboard" className="dashboard-img" fluid />
           </Col>
           <Col xs={12} md={8} lg={6} className="align-self-center mt-5">
-            <h4>To access our admin dashboard, please follow these steps:</h4>
+            <h4>To test our Admin dashboard, you can follow these steps:</h4>
           </Col>
           <Col xs={12} sm={7} md={8} lg={4} className="align-self-center mt-3">
             <ol className="text-start">
@@ -52,10 +51,13 @@ const DashboardSection = React.forwardRef((props, ref) => {
                   admin login page.
                 </a>
               </li>
-              <li>Enter the email address "test@test.com" as your username.</li>
+              <li>Use the email address "test@test.com" as your username.</li>
               <li>Input "123" as the password.</li>
               <li>Click on the login button or submit the form.</li>
             </ol>
+            <p>
+              Now you can access the admin dashboard with the provided login data. Happy testing!
+            </p>
           </Col>
         </div>
       </Row>
